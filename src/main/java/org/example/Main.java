a34251639b751fbae2abddcae63a75eaa38c3efb
sahelassadi <sahel552010@hotmail.com>
2025-04-29 15:02:11 -0400

    Add selectionSort in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {3, 1, 5, 2, 1, 4};
-        bubbleSort(nums);              // {1, 1, 2, 3, 4, 5}
+        selectionSort(nums);              // {1, 1, 2, 3, 4, 5}
 
         System.out.println(Arrays.toString(nums));
 
@@ -50,6 +50,29 @@ public class Main {
                     nums[j + 1] = temp;
                 }
             }
+        }
+    }
+    /*
+ 315214
+ 135214
+ 115234
+ 112534
+ 112354
+ 112345
+    */
+
+    public static void selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            // swap nums[i] and nums[minIdx]
+            int temp = nums[i];
+            nums[i] = nums[minIdx];
+            nums[minIdx] = temp;
         }
     }
 }
